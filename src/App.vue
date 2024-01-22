7bb3b49cd02abb344e6350fd1f3c19041acbc853
mazzobeg <mazzobel.gvn@gmail.com>
2024-01-22 13:03:03 +0100

    Update App.vue and main.js files, add new font and image assets

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="container pt-5">
+  <nav class="navbar" style="height: 50px; background-color: #3D5235;">
+  </nav>
+
+  <img class="mx-auto d-block" src="@/assets/header.png" alt="" srcset="" width="100" style="position: relative; bottom: 40px;">
+
+  <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <div class="progress mb-3" role="progressbar" aria-label="Example 20px high" :aria-valuenow="step/3*100" aria-valuemin="0"
-          aria-valuemax="100" style="height: 20px">
-          <div class="progress-bar" :style="`width: ${step/3*100}%`"></div>
+        <div class="progress mb-3" role="progressbar" aria-label="Example 20px high" :aria-valuenow="step / 3 * 100"
+          aria-valuemin="0" aria-valuemax="100" style="height: 20px">
+          <div class="progress-bar" :style="`width: ${step / 3 * 100}%`"></div>
         </div>
         <CharacterForm1 v-if="step === 0" @update:character-name="name => display(name)" @next="step++" />
         <CharacterForm2 v-if="step === 1" @update:race="race => displayRace(race)" @next="step++" @previous="step--"
           @update:classe="classe => displayClasse(classe)" />
-        <CharacterForm3 v-if="step === 2" @next="step++" @previous="step--"/>
-        <CharacterForm4 v-if="step === 3" @next="step++" @previous="step--"/>
-        </div>
+        <CharacterForm3 v-if="step === 2" @next="step++" @previous="step--" />
+        <CharacterForm4 v-if="step === 3" @next="step++" @previous="step--" />
+      </div>
       <div class="col-md-6 ps-md-5 mt-5 mt-md-0">
         <div class="bg-light border border-dark rounded" >
-        <CharacterFile :character-name="characterName" :race="race" :caracteristiques="caracteristiques"
-          :competences="competences" :equipements="equipements" :bonus_maitrise="bonus_maitrise" />
+          <CharacterFile :character-name="characterName" :race="race" :caracteristiques="caracteristiques"
+            :competences="competences" :equipements="equipements" :bonus_maitrise="bonus_maitrise" />
         </div>
       </div>
-      
+
     </div>
   </div>
 </template>
@@ -65,13 +70,13 @@ const competences = ref({
 
 const race = ref('')
 const classe_payload = {
-    "classe" : null,
-    "équipements" : {},
-    "compétences" : {},
-    "maitrise": {},
-    "sauvegarde": [],
-    "capacités" : [],
-    "bonus_maitrise" : ""
+  "classe": null,
+  "équipements": {},
+  "compétences": {},
+  "maitrise": {},
+  "sauvegarde": [],
+  "capacités": [],
+  "bonus_maitrise": ""
 };
 
 const bonus_maitrise = ref(0);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/main.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import store from './store'
 
